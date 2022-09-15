465b3d9a99b66cc907cf7ff103e729a84be427e7
kost333 <levonkostanyan2021@mail.ru>
2022-09-15 17:55:30 +0400

    create states inputs

--- src/component/register/register.jsx
+++ src/component/register/register.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './register.module.css';
 
 const Register = () => {
+
+    const [state, setState] = useState({
+        name: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const signIn = () => {
+        if (state.confirmPassword === state.password) {
+            setState({
+                name: state.name,
+                username: state.username,
+                email: state.email,
+                password: '',
+                confirmPassword: '',
+            })
+            console.log(state);
+        }
+    }
+
     return (
         <div className={style.window}>
             <div className={style.innerWindow}>
@@ -19,23 +41,29 @@ const Register = () => {
                     </div>
                     <div className={style.inputName}>
                         <div className={style.NAME}>NAME</div>
-                        <input type="text" placeholder="Your full name" className={style.name}/>
+                        <input type="text" placeholder="Your full name" className={style.name} value={state.name}
+                               onChange={e => setState({...state, name: e.target.value})}/>
                     </div>
                     <div className={style.InputUsername}>
                         <div className={style.USERNAME}>USERNAME</div>
-                        <input type="text" placeholder="Username" className={style.username}/>
+                        <input type="text" placeholder="Username" className={style.username} value={state.username}
+                               onChange={e => setState({...state, username: e.target.value})}/>
                     </div>
                     <div className={style.inputEmail}>
                         <div className={style.EMAIL}>EMAIL</div>
-                        <input type="email" placeholder="Email address" className={style.email}/>
+                        <input type="email" placeholder="Email address" className={style.email} value={state.email}
+                               onChange={e => setState({...state, email: e.target.value})}/>
                     </div>
                     <div className={style.inputPassword}>
                         <div className={style.PASSWORD}>PASSWORD</div>
-                        <input type="password" placeholder="Password" className={style.password}/>
+                        <input type="password" placeholder="Password" className={style.password} value={state.password}
+                               onChange={e => setState({...state, password: e.target.value})}/>
                     </div>
                     <div className={style.inputConfirmPassword}>
                         <div className={style.CONFIRM_PASSWORD}>Confirm Password</div>
-                        <input type="password" placeholder="Confirm_Password" className={style.confirm_password}/>
+                        <input type="password" placeholder="Confirm_Password" className={style.confirm_password}
+                               value={state.confirmPassword}
+                               onChange={e => setState({...state, confirmPassword: e.target.value})}/>
                     </div>
                     <div className={style.checkbox}>
                         <input type="checkbox" className={style.check}/>
@@ -44,6 +72,11 @@ const Register = () => {
                     <div className={style.buttonCreate}>
                         <button className={style.buttonInput}>
                             CREATE ACCOUNT
+                        </button>
+                    </div>
+                    <div className={style.buttonCreate}>
+                        <button className={style.buttonInput} onClick={signIn}>
+                            SIGN IN
                         </button>
                     </div>
                 </div>
